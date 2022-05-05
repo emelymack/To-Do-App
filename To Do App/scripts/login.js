9a1a92f0620e17a852d1f71cf189c14e84b36609
emelymack <emelymack@gmail.com>
2022-05-05 17:00:52 -0300

    Agrego funcionalidades login, falta terminar

--- To Do App/scripts/login.js
+++ To Do App/scripts/login.js
@@ -12,13 +12,25 @@ window.addEventListener('load', function () {
        event.preventDefault()
 
        // creamos el cuerpo de la request
-
+        let bodyReq = JSON.stringify({
+            "email": email.value,
+            "password": password.value
+        })
+        
        // configuramos la request del Fetch
+       const settings = {
+        method: "POST",
+        body: bodyReq,
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
 
        // llamamos a la funcion q hace la consulta (realizarLogin)
+        realizarLogin(settings);
 
        // reseteamos el form
-       // form.reset()
+        form.reset()
 
 
     });
@@ -30,9 +42,15 @@ window.addEventListener('load', function () {
     function realizarLogin(settings) {
        
         // fetch , enviando los settings como 2do param
-
-
-
+        fetch(`${url}/users/login`, settings)
+        .then( response => response.json() )
+        .then( data => {
+            console.log("Realizando login...");
+            console.log(data);
+            if(data.jwt){
+                
+            }
+        } )
         
     };
 
